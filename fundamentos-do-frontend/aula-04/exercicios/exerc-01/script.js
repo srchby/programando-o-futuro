ab4083b8c8c8ec3cda446e2d0988421903ef2501
srchby <raidentsuji@gmail.com>
2025-05-30 18:52:13 -0300

    aula 04

---
+++ fundamentos-do-frontend/aula-04/exercicios/exerc-01/script.js
@@ -0,0 +1,6 @@
+function tip() {
+  image = document.getElementById("image");
+  image.src = "https://i.ytimg.com/vi/_Xk3Rn3jGDM/maxresdefault.jpg";
+  document.getElementById("tip").innerHTML =
+    "Para fazer ovo cozido: Espere a água ferver, então apague o fovo e deixe o ovo na água quente por 10 minutos";
+}
