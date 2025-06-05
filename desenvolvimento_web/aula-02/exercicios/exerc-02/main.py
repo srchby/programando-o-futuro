48f97e66504ecac49b0068599bc41b7d768d45e7
srch <raidentsuji@gmail.com>
2025-06-04 21:55:23 -0300

    exerc 02 - aula 02

--- desenvolvimento_web/aula-02/exercicios/exerc-02/main.py
+++ desenvolvimento_web/aula-02/exercicios/exerc-02/main.py
@@ -1,25 +1,29 @@
-class staff:
+class Staff:
     def __init__(self, name, salary, age):
         self.name = name
         self.salary = salary
         self.age = age
     
     def print(self):
-        return f"Nome: {self.name} \nSalário: {self.salary} \nAge: {self.age}" 
-    
+        return f"Nome: {self.name} \nSalário: {self.salary} \nAge: {self.age}"
 
-class manager(staff):
+class Manager(Staff):
     def __init__(self, name, salary, age, secretary):
         super().__init__(name, salary, age)
         self.secretary = secretary
         
     def apresentar(self):
-        return f"{print()} \nSecretário: {self.secretary}"
+        return f"{super().print()} \nSecretário: {self.secretary}\n"
 
-class analyst(staff):
+class Analyst(Staff):
     def __init__(self, name, salary, age, area):
         super().__init__(name, salary, age)
         self.area = area
 
     def apresentar(self):
-        return f"{super().print()} \nÁrea {self.area}"
+        return f"{super().print()} \nÁrea {self.area}\n"
+
+workers = [Manager("B","3000","30","C"), Analyst("D","5000","50","E")]
+
+for w in workers:
+    print(w.apresentar())
