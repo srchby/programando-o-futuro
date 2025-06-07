cbb4181b012feedbafa0d60ce03f8d7b75452f31
srchby <raidentsuji@gmail.com>
2025-06-07 15:46:12 -0300

    aula 05 - exercicio

--- desenvolvimento_web/aula-05/exercicios/exerc-01/app.py
+++ desenvolvimento_web/aula-05/exercicios/exerc-01/app.py
@@ -1,14 +1,63 @@
 # https://www.codementor.io/@garethdwyer/building-a-crud-application-with-flask-and-sqlalchemy-dm3wv7yu2
 
 from flask import Flask, render_template, request, redirect
-from models import db, Client
+from models import db, Client, Order
+from datetime import date, datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///client.db'
 db.init_app(app)
 
+
 with app.app_context():
     db.create_all()
+    test_clients = [
+        Client(email='abc@abc.com', name='abc'),
+        Client(email='bbc@abc.com', name='bbc'),
+    ]
+    test_orders = [
+        Order(id=1, date="12032025", price=2.00, client_email='abc@abc.com'),
+        Order(id=2, date="21052025", price=2.00, client_email='abc@abc.com'),
+        Order(id=3, date="21052025", price=2.00, client_email='bbc@abc.com')
+    ]
+
+    if not Client.query.first():
+        db.session.add_all(test_clients)
+        db.session.add_all(test_orders)
+        
+    db.session.commit()
+    
+    print(Order.query.all())
+    print(Client.query.all())
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    email = request.form.get('email')
+    client = Client.query.filter_by(email=email).first()
+    if client:
+        db.session.delete(client)
+        db.session.commit()
+    return redirect('/')
+
+@app.route("/update", methods=["POST"])
+def update():
+    oldemail = request.form.get("oldemail")
+    newemail = request.form.get("newemail")
+    newname = request.form.get("newname")
+    newphone = request.form.get("newphone")
+    client = Client.query.filter_by(email=oldemail).first()
+    if client:
+        client.email = newemail
+        client.name = newname
+        client.phone = newphone
+        db.session.commit()
+    return redirect("/")
+
+@app.route('/orders', methods=['GET'])
+def orders():
+    orders = Order.query.all()
+    clients = Client.query.all()
+    return render_template('orders.html', orders=orders, clients=clients)
 
 @app.route('/new', methods=['GET','POST'])
 def new():
--- desenvolvimento_web/aula-05/exercicios/exerc-01/models.py
+++ desenvolvimento_web/aula-05/exercicios/exerc-01/models.py
@@ -1,8 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date, datetime
 
 db = SQLAlchemy()
 
 class Client(db.Model):
     email = db.Column(db.String(254), primary_key=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.String(14), nullable=False)
+    phone = db.Column(db.String(14))
+    
+    orders = db.relationship('Order', back_populates='client', cascade='all, delete-orphan')
+    
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, default=date.today)
+    price = db.Column(db.Float, nullable=False)
+    
+    client_email = db.Column(db.String(254), db.ForeignKey('client.email'), nullable=False)
+    client=db.relationship('Client', back_populates='orders')
+    
