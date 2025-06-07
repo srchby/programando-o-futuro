4e2ee1de17d85f24fc7264414cedca20c0e7918a
srchby <raidentsuji@gmail.com>
2025-06-07 10:18:52 -0300

    atividade 06/06

--- desenvolvimento_web/aula-04/exercicios/exerc-01/app.py
+++ desenvolvimento_web/aula-04/exercicios/exerc-01/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 products = []
 
 @app.route('/', methods=['GET','POST'])
 def manage_products():
     if request.method == 'POST':
-        name = request.form.get('name')
-        price = request.form.get('price')
-        description = request.form.get('description')
-        products.append({'name': name, 'price': price, 'description': description  })
-        
+        name = request.form.get('name', '').strip()
+        price = request.form.get('price', '').strip()
+        price = int(price)
+        description = request.form.get('description', '').strip()
+        if name and price and description:
+            products.append({'name': name, 'price': price, 'description': description})
+        return redirect(url_for('manage_products')) 
     return render_template('index.html', products=products)
 
 if __name__ == '__main__':
